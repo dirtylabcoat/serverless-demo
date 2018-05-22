13f1e05bea9f5cd535c6f40d5e88a17a6277ddd4
Daniel Löfquist <daniel@lofquist.org>
2018-05-22 08:44:29 +0200

    Tweaks in scripts

--- awsnthprime/aws-api-gateway-setup.sh
+++ awsnthprime/aws-api-gateway-setup.sh
@@ -7,18 +7,12 @@ TMP_FILE=tmp.log
 
 ## Create API NthPrime
 $AWS apigateway create-rest-api --name $API_NAME > $TMP_FILE
-API_ID=$(grep "\"id\":" $TMP_FILE | sed 's/[[:space:]]*\"id\": \"\(.*\)\"/\1/g')
-echo 1
+API_ID=$(grep "\"id\":" $TMP_FILE | sed 's/[[:space:]]*\"id\": \"\(.*\)\",\?.*/\1/g')
 ## Create resource /nth-prime
 $AWS apigateway get-resources --rest-api-id $API_ID > $TMP_FILE
-ROOT_RESOURCE_ID=$(grep "\"id\":" $TMP_FILE | sed 's/[[:space:]]*\"id\": \"\(.*\)\"/\1/g')
-echo 2
+ROOT_RESOURCE_ID=$(grep "\"id\":" $TMP_FILE | sed 's/[[:space:]]*\"id\": \"\(.*\)\",\?.*/\1/g')
 $AWS apigateway create-resource --rest-api-id $API_ID --parent-id $ROOT_RESOURCE_ID --path-part 'nth-prime' > $TMP_FILE
-exit 1;
-RESOURCE_ID=$(grep "\"id\":" $TMP_LOG | sed 's/[[:space:]]*\"id\": \"\(.*\)\"/\1/g')
-echo $RESOURCE_ID
+RESOURCE_ID=$(grep "\"id\":" $TMP_LOG | sed 's/[[:space:]]*\"id\": \"\(.*\)\",\?.*/\1/g')
 ## Create method POST
-$AWS apigateway put-method --rest-api-id $API_ID --resource-id $RESOURCE_ID --http-method POST --authorization-type NONE
-
+#$AWS apigateway put-method --rest-api-id $API_ID --resource-id $RESOURCE_ID --http-method POST --authorization-type NONE
 rm -f $TMP_LOG
-echo 4
--- awsnthprime/aws-lambda-setup.sh
+++ awsnthprime/aws-lambda-setup.sh
@@ -4,6 +4,8 @@ LAMBDA_EXEC_ROLE=NthPrimeLambdaExecRole
 LAMBDA_AWS_POLICY=arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 LAMBDA_FUNCTION_NAME=NthPrimeLambdaFunction
 LAMBDA_FUNCTION_DESCRIPTION="Demo for HiQ Tech Away #3"
+JAR_FILE=target/awsnthprime.jar
+TMP_FILE=tmp.log
 
 # Setup IAM role for Lambda execution
 
@@ -28,17 +30,17 @@ CHK_FUNCTION_EXISTS=$(echo $?)
 if [ $CHK_FUNCTION_EXISTS -eq 255 ]; then
     ## Create function and upload JAR
     echo "Function [$LAMBDA_FUNCTION_NAME] doesn't exist. Attempting code upload and creation..."
-    $AWS lambda create-function --function-name $LAMBDA_FUNCTION_NAME --runtime java8 --role $LAMBDA_EXEC_ROLE_ARN --handler se.hiq.techaway.slsdemo.awsnthprime.Primer --zip-file fileb://target/awsnthprime.jar --description "$LAMBDA_FUNCTION_DESCRIPTION"
+    $AWS lambda create-function --function-name $LAMBDA_FUNCTION_NAME --runtime java8 --role $LAMBDA_EXEC_ROLE_ARN --handler se.hiq.techaway.slsdemo.awsnthprime.Primer --zip-file fileb://$JAR_FILE --description "$LAMBDA_FUNCTION_DESCRIPTION"
 else
     ## Update function with current JAR
     echo "Function [$LAMBDA_FUNCTION_NAME] already exists. Attempting code upload and update..."
-    $AWS lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://target/awsnthprime.jar
+    $AWS lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://$JAR_FILE
 fi
 
 # Test invoke Lambda function
 
-echo "Invoke Lambda function [$LAMBDA_FUNCTION_NAME] with input 10..."
-$AWS lambda invoke --invocation-type RequestResponse --function-name NthPrimeLambdaFunction --region eu-central-1 --log-type Tail --payload '{"n":10}' out.tmp
+echo "Invoke Lambda function [$LAMBDA_FUNCTION_NAME] with input '{\"n\":10}'..."
+$AWS lambda invoke --invocation-type RequestResponse --function-name NthPrimeLambdaFunction --region eu-central-1 --log-type Tail --payload '{"n":10}' $TMP_LOG
 echo "Result of invocation:"
-cat out.tmp | python -m json.tool && echo ""
-rm out.tmp
+cat $TMP_LOG | python -m json.tool && echo ""
+rm -f $TMP_LOG
